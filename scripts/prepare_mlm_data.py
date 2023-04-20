1c2b2b0c37cf365623643e0b7cfadf87c3834472
Sbansal97 <srijanb@andrew.cmu.edu>
2023-04-19 20:31:50 -0400

    updated adapterbias eval

---
+++ scripts/prepare_mlm_data.py
@@ -0,0 +1,53 @@
+from datasets import load_dataset
+import os
+import sys
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", required=True)    
+    args = parser.parse_args()
+    return args
+
+def get_mlm():  
+    args = get_args()
+
+    if args.dataset_name == 'bias-bios':
+        dataset = load_dataset('parquet', data_files={
+            'train' : 'JustiSparse/data/bias-bios/biasbios_train.pq', 
+            'validation' : 'JustiSparse/data/bias-bios/biasbios_val.pq',
+            'test' : 'JustiSparse/data/bias-bios/biasbios_test.pq'}
+            )
+        text_field = 'text'
+        train_texts  = list(dataset['train'][text_field])
+        valid_texts = list(dataset['validation'][text_field])
+        test_texts = list(dataset['test'][text_field])
+
+    elif args.dataset_name == 'ontonotes':        
+        dataset = load_dataset("conll2012_ontonotesv5", "english_v12")
+        import pdb
+        pdb.set_trace()                
+    else:
+        raise NotImplementedError
+
+
+    print (f"length : train {len(train_texts)}, val : {len(valid_texts)}, test : {len(test_texts)}")
+
+    data_dir = os.path.join('data', dataset_name, 'mlm')
+    os.path.makedirs(data_dir, exists_ok=True)
+
+    with open(os.path.join(data_dir, 'train.txt', 'w')) as f:
+        for t in train_texts:
+            f.write(t.strip()+'\n')
+
+    with open(os.path.join(data_dir, 'validation.txt', 'w')) as f:
+        for t in valid_texts:
+            f.write(t.strip()+'\n')
+
+    with open(os.path.join(data_dir, 'test.txt', 'w')) as f:
+        for t in test_texts:
+            f.write(t.strip()+'\n')
+
+if __name__ == "__main__":
+    get_mlm()
