4aee075c7b9d9d4e89847ede8b7394dbd2ae1082
Sbansal97 <srijanb@andrew.cmu.edu>
2023-04-23 04:31:44 -0400

    slurm

--- run_intrinsic.sh
+++ run_intrinsic.sh
@@ -1,8 +1,8 @@
 PEFT=$1
 GPU_ID=$2
 DEBIAS=$3
-AXIS=gender
-DATASET=bias-bios
+AXIS=$4 # gender, group
+DATASET=$5 #bias-bios, gab
 
 cache_dir=~/.cache
 train_path=data/${DATASET}/train.jsonl
@@ -109,5 +109,32 @@ elif [[ $DEBIAS == "adv" ]];then
             --sparse_ft_max_steps_per_iteration 10000 \
             --adv_debias \
             --load_best_model_at_end > models/${PEFT}/${DEBIAS}/${AXIS}/${DATASET}/training.log
-    fi
+    else:
+        python run_intrinsic.py \
+            --model_name_or_path bert-base-uncased \
+            --protected_attribute_column 'g' \
+            --train_file $train_path \
+            --validation_file $val_path \
+            --output_dir models/${PEFT}/${DEBIAS}/${DATASET}/${AXIS} \
+            --do_train \
+            --do_eval \
+            --log_level 'info' \
+            --preprocessing_num_workers 4 \
+            --per_device_train_batch_size 32 \
+            --per_device_eval_batch_size 32 \
+            --gradient_accumulation_steps 4 \
+            --max_seq_length 256 \
+            --save_steps 1000 \
+            --overwrite_output_dir \
+            --learning_rate 5e-5 \
+            --evaluation_strategy steps \
+            --max_steps 10000 \
+            --eval_steps 1000 \
+            --train_adapter \
+            --peft ${PEFT} \
+            --adapter_config ${PEFT} \
+            --cache_dir $cache_dir \
+            --adv_debias \
+            --load_best_model_at_end > models/${PEFT}/${DEBIAS}/${AXIS}/${DATASET}/training.log
 fi
+
---
+++ run_slurm.sh
@@ -0,0 +1,11 @@
+!/bin/sh
+
+# SBATCH --job-name=peft-debias
+# SBATCH --mem 32G
+# SBATCH -t 0
+# SBATCH --gres=gpu:A6000:1
+# SBATCH --exclude=tir-0-32
+# SBATCH --ntasks 1
+# SBATCH --output /projects/tir6/general/srijanb/Spr23/JustiSparse/slurm_logs/log-%x-%J.txt
+
+bash run_intrinsic.sh pfeiffer 0 
